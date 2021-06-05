4b63ddcf77c2b69aa3bf35d3917a0690a0a3b522
kzarama <zaramaluna1999@hotmail.com>
2021-06-05 18:20:23 -0500

    packages updated

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -18,6 +18,8 @@ import {
   Box,
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
+import Check from "@material-ui/icons/Check";
+// import Accessibility from "@material-ui/icons/Cancel";
 import React, { useState } from "react";
 
 const theme = createMuiTheme({
@@ -85,8 +87,19 @@ const useStyles = makeStyles({
   },
   list_class: {
     width: "50%",
+    height: "55vh",
     margin: "auto",
     marginTop: "20px",
+    overflowY: "scroll",
+    paddingRight: "10px",
+  },
+  "@global": {
+    "*::-webkit-scrollbar": {
+      width: "5px",
+    },
+    "*::-webkit-scrollbar-thumb": {
+      backgroundColor: "#00ccff",
+    },
   },
   list_item_class: {
     border: "4px solid #00ccff",
@@ -98,7 +111,13 @@ const useStyles = makeStyles({
       background: "#121212",
     },
   },
-  delete_icon_class: {
+  task_done_class: {
+    textDecoration: "line-through",
+    textDecorationColor: "red",
+    lineHeight: "50px",
+    textDecorationWidth: "50px",
+  },
+  icon_class: {
     color: "#00ccff",
     fontSize: "30px",
   },
@@ -148,6 +167,7 @@ const useStyles = makeStyles({
 
 interface ITask {
   taskName: string;
+  taskDone: boolean;
 }
 
 function App(): JSX.Element {
@@ -161,20 +181,28 @@ function App(): JSX.Element {
   const [tasks, setTasks] = useState<ITask[]>([]);
 
   const handleSubmit = (): void => {
-    const newTask: ITask[] = [...tasks, { taskName: inputTask }];
+    const newTask: ITask[] = [
+      ...tasks,
+      { taskName: inputTask, taskDone: false },
+    ];
     setTasks(newTask);
     setInputTask("");
+  };
+
+  const toggleDone = (i: number): void => {
+    const taskDone: ITask[] = [...tasks];
+    taskDone[i].taskDone = !taskDone[i].taskDone;
+    setTasks(taskDone);
   };
 
   const handleDelete = (i: number): void => {
     const newTasks: ITask[] = [...tasks];
     newTasks.splice(i, 1);
     setTasks(newTasks);
-    console.log(tasks);
   };
 
   const setUpdateTaskMethod = (name: string): void => {
-    setUpdateTask({ taskName: name });
+    setUpdateTask({ taskName: name, taskDone: tasks[idSelected].taskDone });
   };
 
   const handleUpdate = (): void => {
@@ -226,10 +254,16 @@ function App(): JSX.Element {
                 className={classes.list_item_class}
                 onClick={() => handleOpen(i)}
               >
-                <ListItemText primary={task.taskName} />
+                <ListItemText
+                  className={tasks[i].taskDone ? classes.task_done_class : ""}
+                  primary={task.taskName}
+                />
                 <ListItemSecondaryAction>
+                  <IconButton onClick={() => toggleDone(i)}>
+                    <Check className={classes.icon_class} />
+                  </IconButton>
                   <IconButton onClick={() => handleDelete(i)}>
-                    <DeleteIcon className={classes.delete_icon_class} />
+                    <DeleteIcon className={classes.icon_class} />
                   </IconButton>
                 </ListItemSecondaryAction>
               </ListItem>
